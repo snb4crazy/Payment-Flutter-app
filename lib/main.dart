94af039780ca2b47174b7c6f2d0cfbbf98e40b2f
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-09 15:46:57 -0500

    Add GET lib to main MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app_from_scratch/screens/my_home_page.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Payment App',
       theme: ThemeData(
