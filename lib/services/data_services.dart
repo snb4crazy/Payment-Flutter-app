01464da24a9fd0760fc44b58da4d06322b1916a8
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-10 21:03:59 -0500

    DataServices class to access json data

---
+++ lib/services/data_services.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+class DataServices {
+  Future<List<dynamic>> getUsers() async {
+    var info = rootBundle.loadString('json/data.json');
+    var list = json.decode(await info);
+    return list.map((e) => e).toList();
+  }
+}
