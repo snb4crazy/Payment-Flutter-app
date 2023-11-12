5e68ecebc65d4596a13540de6007151e8e113a94
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-11 21:09:07 -0500

    DataServices: integrate HTTP API & refactoring

--- lib/services/data_services.dart
+++ lib/services/data_services.dart
@@ -1,12 +1,34 @@
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
+import 'package:payment_app_from_scratch/models/data_model.dart';
 
 class DataServices {
-  Future<List<dynamic>> getUsers() async {
+  final String _baseUrl = 'http://host/api/';
+
+  Future<List<DataModel>> getUsers() async {
+    var apiUrl = '/getRoute';
+    http.Response response = await http.get(Uri.parse(_baseUrl + apiUrl));
+    try {
+      if (response.statusCode == 200) {
+        //success
+        print(await json.decode(response.body));
+        List<dynamic> list = await json.decode(response.body);
+        //worked before creating a Model
+        //return list.map((e) => e).toList();
+        return list.map((e) => DataModel.fromJson(e)).toList();
+      } else {
+        //failed
+        return <DataModel>[];
+      }
+    } catch (e) {
+      print(e);
+      return <DataModel>[];
+    }
+    //below mapping for local json, before integration of the API
     var info = rootBundle.loadString('json/data.json');
     var list = json.decode(await info);
     return Future.delayed(
         const Duration(seconds: 2), () => list.map((e) => e).toList());
-    //return ;
   }
 }
